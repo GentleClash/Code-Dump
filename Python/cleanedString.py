d6fdef0934d5b166e603706b25803fa65d70c647
Ayush <ayushbajpai016@gmail.com>
2023-03-25 22:35:39 +0530

    cleanedString program added

---
+++ Python/cleanedString.py
@@ -0,0 +1,17 @@
+"""Clean the following text and find the most frequent word.
+    string = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+"""
+
+from re import sub, split
+from collections import Counter
+
+string="%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching"
+
+cleanString=sub(r"[^\w^\s]", "", string) #Removes any character that is not alphanum or whitespace
+
+print(cleanString)
+wordCount=Counter([_ for _ in cleanString.split(" ")]) #Dictionary of count of words
+
+word=[_ for _,count in wordCount.items() if count==max(wordCount.values())] #List of word(s) with most count
+
+print((word))
